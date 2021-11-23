b061c5ef67d1f6731af3b7d351de98cc0bf1fbfa
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-11-23 13:08:48 -0600

    3

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from '../Shared/Navigation/Navigation';
 import './Home.css';
+import Services from './Services/Services';
 
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <Services></Services>
         </div>
     );
 };
