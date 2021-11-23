31f9c02ab428359d835bd039da7cdf5473cf2742
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-11-23 12:41:59 -0600

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import './App.css';
+import Home from "./Pages/Home/Home";
+import Navigation from "./Pages/Shared/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/navigation">
+            <Navigation />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+
+      </Router>
+      
+
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../Shared/Navigation/Navigation';
+import './Home.css';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+        </div>
+    );
+};
+
+export default Home;
